1afcd031291a6faa3f4160dc50d58a7ca9dec094
Himasara1212 <dinuhima0512@gmail.com>
2026-07-03 17:18:32 +0530

    Start create pigeon service project

---
+++ PigeonService.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PigeonService {
+    static Scanner input = new Scanner(System.in);
+
+    static String username = "danujav";
+    static String password = "1234";
+
+    public static void main(String [] args){
+        loginpage();
+    }
+
+    public static void loginpage() {
+       
+    }
+}
