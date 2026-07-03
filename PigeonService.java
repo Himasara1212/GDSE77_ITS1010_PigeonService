1cbe39357e62258f1858f2f914c6d94c322965af
Himasara1212 <dinuhima0512@gmail.com>
2026-07-03 18:04:09 +0530

    Create Login Page

--- PigeonService.java
+++ PigeonService.java
@@ -11,6 +11,41 @@ public class PigeonService {
     }
 
     public static void loginpage() {
-       
+       System.out.println("+======================================================+");
+       System.out.println("|                      LOGIN PAGE                      |");
+       System.out.println("+======================================================+\n");
+
+       String name = "";
+
+        while (!username.equals(name)) {
+            System.out.print("User Name : ");
+            name = input.nextLine();
+
+            if (name.equals(username)) {
+                System.out.println("Login is Success");
+            } else {
+                System.out.println("User name is incorrect. Please try again!\n");
+            }
+        }
+
+        String pw = "";
+
+        while (!password.equals(pw)) {
+            System.out.print("Password : ");
+            pw = input.nextLine();
+
+            if (pw.equals(password)) {
+                System.out.println("Password is Correct");
+            } else {
+                System.out.println("Password is invalid. Please try again!\n");
+            }
+        }
+        homepage();
+    }
+
+    private static void homepage() {
+       System.out.println("\n+======================================================+");
+       System.out.println("|                       HOME PAGE                      |");
+       System.out.println("+======================================================+\n");
     }
 }
